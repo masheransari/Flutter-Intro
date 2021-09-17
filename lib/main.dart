ecc83b22b71ed6afd9a20351a93cff4a7ddfdc13
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-09-17 16:13:01 +0500

    Added App bar using Scaffold while adding body as a centerText container

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,20 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(MaterialApp(
-    title: "My First App",
-    debugShowCheckedModeBanner: false,
-    home: Material(
-      color: Colors.amberAccent,
-      child: Center(
-        child: Text(
-          "Hello World",
-          textDirection: TextDirection.ltr,
+      title: "My First App",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
         ),
-      ),
-    ),
-  ));
+        body: Material(
+          color: Colors.amberAccent,
+          child: Center(
+            child: Text(
+              "Hello World",
+              textDirection: TextDirection.ltr,
+            ),
+          ),
+        ),
+      )));
 }
