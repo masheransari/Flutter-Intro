0bbefe1499db94c08467ed6e4ff48bf3156e0be5
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-09-17 16:40:28 +0500

    Added fat Arrow while removing Curly Bracket from main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(MaterialApp(
-      title: "My First App",
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-          backgroundColor: Colors.amber,
-        ),
-        body: Material(
-          color: Colors.amberAccent,
-          child: Center(
-            child: Text(
-              "Hello World",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(fontSize: 40.0, color: Colors.white),
-            ),
+void main() => runApp(MaterialApp(
+    title: "My First App",
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Material(
+        color: Colors.amberAccent,
+        child: Center(
+          child: Text(
+            "Hello World",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 40.0, color: Colors.white),
           ),
         ),
-      )));
-}
+      ),
+    )));
