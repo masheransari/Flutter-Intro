f9729019a19d08c37989286c771ad8a91115ee6b
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-09-22 13:17:09 +0500

    adding class in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import "package:flutter/material.dart";
 
-void main() => runApp(MaterialApp(
-    title: "My First App",
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My First App"),
-        backgroundColor: Colors.amber,
-      ),
-      body: Material(
-        color: Colors.amberAccent,
-        child: Center(
-          child: Text(
-            "Hello World",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 40.0, color: Colors.white),
+void main() => runApp(myApp());
+
+//Defining a class.
+//class as subClass
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My First App",
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First App"),
+            backgroundColor: Colors.amber,
           ),
-        ),
-      ),
-    )));
+          body: Material(
+            color: Colors.amberAccent,
+            child: Center(
+              child: Text(
+                "Hello World",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 40.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ));
+  }
+}
